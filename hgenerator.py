172594fa25f77843dabf376e0e4f1515174686cd
VivaRafael <42812411+VivaRafael@users.noreply.github.com>
2019-10-19 15:07:45 +0100

    Create hgenerator.py

---
+++ hgenerator.py
@@ -0,0 +1,39 @@
+def hGenerator(graph):
+    f = open("h.txt", "w")
+    f.write("[")
+    for i in range(1, 114):
+        distances = dijkstra(graph, i)
+        f.write("[")
+        for minimum_cost in distances:
+            f.write(str(minimum_cost) + ", ")
+        f.write("], ")
+    f.write("]")
+    f.close()
+
+
+def dijkstra(graph, source):
+    dist = list()
+    # previous = list()
+    queue = list()
+    for i in range(1, 114):
+        dist.append(200)
+        queue.append(i)
+
+    dist[source-1] = 0
+    while queue:
+        i = 0
+        j = 0
+        node_with_minimum_d = queue[0]
+        for node in queue:
+            if dist[node-1] < dist[node_with_minimum_d-1]:
+                node_with_minimum_d = node
+                j = i
+            i += 1
+        queue.pop(j)
+
+        for neighbor in graph[node_with_minimum_d]:
+            new_distance = dist[node_with_minimum_d-1] + 1
+            if new_distance < dist[neighbor[1]-1]:
+                dist[neighbor[1]-1] = new_distance
+
+    return dist
