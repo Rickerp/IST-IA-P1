9a5f3aca49a85da4540d1e877bbfea82cc4de2bd
Ricardo Fernandes <ricardo.s.fernandes@tecnico.ulisboa.pt>
2019-10-23 21:54:48 +0100

    Update heuristics

--- assets/hgenerator.py
+++ assets/hgenerator.py
@@ -1,7 +1,7 @@
 def hGenerator(graph):
     f = open("h.txt", "w")
     f.write("[")
-    for i in range(1, 114):
+    for i in range(0, 114):
         distances = dijkstra(graph, i)
         f.write("[")
         for minimum_cost in distances:
@@ -15,25 +15,25 @@ def dijkstra(graph, source):
     dist = list()
     # previous = list()
     queue = list()
-    for i in range(1, 114):
+    for i in range(0, 114):
         dist.append(200)
         queue.append(i)
 
-    dist[source-1] = 0
+    dist[source] = 0
     while queue:
         i = 0
         j = 0
         node_with_minimum_d = queue[0]
         for node in queue:
-            if dist[node-1] < dist[node_with_minimum_d-1]:
+            if dist[node] < dist[node_with_minimum_d]:
                 node_with_minimum_d = node
                 j = i
             i += 1
         queue.pop(j)
 
         for neighbor in graph[node_with_minimum_d]:
-            new_distance = dist[node_with_minimum_d-1] + 1
-            if new_distance < dist[neighbor[1]-1]:
-                dist[neighbor[1]-1] = new_distance
+            new_distance = dist[node_with_minimum_d] + 1
+            if new_distance < dist[neighbor[1]]:
+                dist[neighbor[1]] = new_distance
 
     return dist
